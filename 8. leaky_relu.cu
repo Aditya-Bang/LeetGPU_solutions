8d05e9004577b2287ef149568529bfd48df2aa6a
Aditya-Bang <adityabang15@gmail.com>
2025-07-22 15:00:09 -0400

    solved p8

---
+++ 8. leaky_relu.cu
@@ -0,0 +1,21 @@
+#include <cuda_runtime.h>
+
+__global__ void leaky_relu_kernel(const float* input, float* output, int N) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    float ALPHA = 0.01;
+    float ins = input[idx];
+
+    if (idx < N) {
+        output[idx] = ins * (ins > 0) + ALPHA * ins * (ins <= 0); // try to reduce warp divergence
+    }
+}
+
+// input, output are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float* input, float* output, int N) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N + threadsPerBlock - 1) / threadsPerBlock;
+    
+    leaky_relu_kernel<<<blocksPerGrid, threadsPerBlock>>>(input, output, N);
+    cudaDeviceSynchronize();
+}
